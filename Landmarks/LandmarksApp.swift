a89e9cf540e287c198b922e7758affe5061619ec
蒋远路 <luseike.jiang@wattsonic.com>
2024-10-14 15:50:31 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 远路蒋 on 2024/10/14.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
